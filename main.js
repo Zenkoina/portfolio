a5d73ffc74d7733dd99101dbb516a34be41cef41
Zenkoina <fyleopeo@gmail.com>
2022-10-16 16:17:00 +0200

    finish navbar & continue

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 let prevScrollY = window.scrollY
+let lastLinkClick = performance.now()
 
 window.addEventListener('scroll', () => {
-    console.log(prevScrollY, window.scrollY)
-    if ((window.scrollY > prevScrollY || window.innerHeight < 600) && window.scrollY > 50) {
+    if ((window.scrollY > prevScrollY || window.innerHeight < 600) && window.scrollY > 50 && performance.now() - lastLinkClick > 50) {
         navbar = document.getElementById('navbar')
         navbar.className = 'scrolled'
     } else {
@@ -13,4 +13,10 @@ window.addEventListener('scroll', () => {
     prevScrollY = window.scrollY
 })
 
-console.log(document.getElementById('navbar').getElementsByTagName("a"))
+for (let index = 0; index < document.getElementById('navbar').getElementsByTagName("a").length; index++) {
+    const link = document.getElementById('navbar').getElementsByTagName("a")[index];
+
+    link.addEventListener('click', () => {
+        lastLinkClick = performance.now()
+    })
+}
