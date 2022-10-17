3f33bbb99547510718bb16a6a5769dd4ffbfdde1
Zenkoina <fyleopeo@gmail.com>
2022-10-17 10:59:45 +0200

    continue

--- main.js
+++ main.js
@@ -2,12 +2,20 @@ let prevScrollY = window.scrollY
 let lastLinkClick = performance.now()
 
 window.addEventListener('scroll', () => {
-    if ((window.scrollY > prevScrollY || window.innerHeight < 600) && window.scrollY > 50 && (performance.now() - lastLinkClick > 50 || window.innerHeight < 600)) {
-        navbar = document.getElementById('navbar')
-        navbar.className = 'scrolled'
+    // on smaller screens hide navbar if scrolled past 50px
+    // on larger screens hide navbar if scrolled past 50px & scrolling direction is downwards & a link wasn't just clicked
+    if (window.innerHeight > 600) {
+        if (window.scrollY > prevScrollY && window.scrollY > 50 && performance.now() - lastLinkClick > 50) {
+            document.getElementById('navbar').className = 'hide'
+        } else {
+            document.getElementById('navbar').className = ''
+        }
     } else {
-        navbar = document.getElementById('navbar')
-        navbar.className = ''
+        if (window.scrollY > 50) {
+            document.getElementById('navbar').className = 'hide'
+        } else {
+            document.getElementById('navbar').className = ''
+        }
     }
 
     prevScrollY = window.scrollY
